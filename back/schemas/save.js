903620aa0f795a7fa09e95ef1862b84d035035f9
Kennedy <kennedy.developper@gmail.com>
2022-01-28 17:35:50 +0300

    all schema

---
+++ back/schemas/save.js
@@ -0,0 +1,18 @@
+// Save Document
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'userId',
+            title: 'UserId',
+            type: 'string'
+        }
+    ]
+}
